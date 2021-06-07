000dfb9027d5c4a09f0d80a85b1a8ce138100586
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-06-07 13:05:34 -0500

    Initial commit

---
+++ src/main/java/com/charles/BCCAIMSWebApp/BccaImsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.charles.BCCAIMSWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BccaImsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BccaImsWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charles/BCCAIMSWebApp/BccaImsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.BCCAIMSWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BccaImsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
