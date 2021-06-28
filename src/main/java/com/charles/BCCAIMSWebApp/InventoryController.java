6b0ad5ea287219aa531661a4e46d5b06f0f7e59a
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2021-06-28 14:29:02 -0500

    Added different pages, user authentication, adding css, connected everything to a database

--- src/main/java/com/charles/BCCAIMSWebApp/InventoryController.java
+++ src/main/java/com/charles/BCCAIMSWebApp/InventoryController.java
@@ -1,10 +1,13 @@
 package com.charles.BCCAIMSWebApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
 
 @Controller
 public class InventoryController {
@@ -12,13 +15,11 @@ public class InventoryController {
     @Autowired
     private InventoryRepository inventoryRepository;
 
+    @Autowired
+    private UserRepository userRepo;
+
     @GetMapping("/add")
-    public String addItem(@RequestParam String item, @RequestParam Integer quantity, @RequestParam Integer lowQuantity) {
-        Inventory inventory = new Inventory();
-        inventory.setItem(item);
-        inventory.setQuantity(quantity);
-        inventory.setLowQuantity(lowQuantity);
-        inventoryRepository.save(inventory);
+    public String addItem() {
         return "add";
     }
 
@@ -32,8 +33,40 @@ public class InventoryController {
         return inventoryRepository.findItemById(id);
     }
 
-    @RequestMapping("/delete")
+    @GetMapping("/delete")
     public String delete() {
         return "delete";
     }
+
+    @GetMapping("/view")
+    public String view() {
+        return "view";
+    }
+
+    @GetMapping("/update")
+    public String update() {
+        return "update";
+    }
+
+    @GetMapping("/register")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup_form";
+    }
+    @PostMapping("/process_register")
+    public String processRegistration(User user){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassowrd = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassowrd);
+        userRepo.save(user);
+        return "process_register";
+    }
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
+    }
+
 }
---
+++ src/test/java/com/charles/BCCAIMSWebApp/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.charles.BCCAIMSWebApp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+        user.setEmail("stuff@basecampcodingacademy.org");
+        user.setPassword("Gamer2020*");
+        user.setFirstName("Charles");
+        user.setLastName("Mardis");
+
+        User savedUser = repo.save(user);
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+}
